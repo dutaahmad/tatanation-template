971beada6a98b37c25b9499c0b05f13de191ee29
dutaahmad <dutaahmadtefur@gmail.com>
2024-03-05 02:16:06 +0700

    init homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"]
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
